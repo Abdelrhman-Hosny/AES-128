48bb812664fd7e899c07b0d16d93b05473139c2f
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-06 07:50:03 +0200

    modify loop limits and rotate word

--- src/encrypt/KeyExpansion.v
+++ src/encrypt/KeyExpansion.v
@@ -17,35 +17,36 @@ rcon[9] = 32'h36_00_00_00;
 
 reg [31:0] words [0:43];
 wire [31:0] temp[0:9];
-// assign input key to words 0 -> 3
 genvar i, rNum, j;
 
+// assign input key to words 0 -> 3
 generate
     for (i = 0; i < 4; i = i + 1) begin
         words[i] = keyIn[i*32 + 31:i*32];
     end
 endgenerate
- 
+
+// set the temp for each time j % 4 == 0 
 generate
     for (rNum = 1; rNum < 11; rNum = rNum + 1) begin
-        // TODO: Add RotWord(temp)
-        // Documentation says temp = SubWord(RotWord(words[rNum]))
-        SBytes #(.NWords(1)) SBytes_block(words[rNum * 4], temp[rNum - 1]);
+        SBytes #(.NWords(1)) SBytes_block({words[rNum * 4][23:0],words[rNum * 4][31:24]}, temp[rNum - 1]);
     end
 endgenerate
 
+// generate the words
 generate
     for (j = 4; j < 44; j = j + 1) begin
-        words[j] = (j % 4 == 0) ? words[j - 4] ^ rcon[j/10] ^ temp[(j - 4)/4]: words[j - 4] ^ words[j - 1] ;
+        words[j] = (j % 4 == 0) ? words[j - 4] ^ rcon[j/4 - 1] ^ temp[(j - 4)/4]: words[j - 4] ^ words[j - 1] ;
     
     end
 endgenerate
 
+// at each clock, output all keys
 always @(posedge clk ) begin
     genvar k;
     generate
-        for (k = 0; k < 10; k = k + 1) begin
-            keyOut[k] = { words[k*4 + 3], words[k*4 + 2], words[k*4 + 1], words[k*4 + 0] };
+        for (k = 1; k < 11; k = k + 1) begin
+            keyOut[k - 1] = { words[k*4 + 3], words[k*4 + 2], words[k*4 + 1], words[k*4 + 0] };
         end
     endgenerate
 end
