1780dc2b1db8adb904256d985d1e347c7e0cb00d
Abdelrhman-Hosny <ahosny2792@gmail.com>
2022-05-06 06:24:57 +0200

    made InvSBytes generic (add parameter NWords)

--- src/decrypt/InvSBytes.v
+++ src/decrypt/InvSBytes.v
@@ -1,11 +1,16 @@
 `include "../InvSBox.v"
 
-module InvSBytes(input [127:0] in, output [127:0] out);
+module InvSBytes
+    # (parameter NWords = 4)
+    (input [NWords * 32 - 1:0] bytes_in, output [NWords * 32 - 1:0] bytes_out);
+
+
 
 genvar i;
 generate
-    for (i = 0; i < 16; i = i + 1) begin
-        InvSBox uut(in[i * 8 + 7:i * 8], out[i * 8 + 7:i * 8]);
+  
+    for (i = 0; i < 4 * NWords; i = i + 1) begin
+        InvSBox uut(bytes_in[i * 8 + 7:i * 8], bytes_out[i * 8 + 7:i * 8]);
     end
 endgenerate
 endmodule
