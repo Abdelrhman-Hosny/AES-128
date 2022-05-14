daf51621056d96d95d4687cf8774008c799517e0
Ammar Nasser <ammarnasser0012@gmail.com>
2022-05-14 02:34:28 +0200

    The remainder of Mul Functions in GF

--- src/util/GFMulFunctions.v
+++ src/util/GFMulFunctions.v
@@ -1,3 +1,5 @@
+
+//Takes One Cycle
 module GFMulBy2(Clk, In, Out);
     input Clk;
     input [7:0] In;
@@ -8,6 +10,7 @@ module GFMulBy2(Clk, In, Out);
                       {In[6:0] , 1'b0};
 endmodule 
 
+//Takes One Cycle
 module GFMulBy3(Clk, In, Out);
     input Clk;
     input [7:0] In;
@@ -19,4 +22,83 @@ module GFMulBy3(Clk, In, Out);
     assign Out = In ^ MulBy2Out;
 endmodule
 
+//Takes One Cycles
+module GFMulBy4(Clk, In, Out);
+    input Clk;
+    input [7:0] In;
+    output [7:0] Out;
+
+    wire [7:0] MulBy2Out;
+
+    GFMulBy2 tu0(Clk, In, MulBy2Out);
+    GFMulBy2 tu1(Clk, MulBy2Out, Out);
+endmodule
+
+//Takes 2 Cycles
+module GFMulBy8(Clk, In, Out);
+    input Clk;
+    input [7:0] In;
+    output [7:0] Out;
+
+    wire [7:0] MulBy4Out;
+
+    GFMulBy4 tu0(Clk, In, MulBy4Out);
+    GFMulBy2 tu1(Clk, MulBy4Out, Out);
+endmodule
+
+// Takes 2 Cycles
+module GFMulBy9(Clk, In, Out);
+    input Clk;
+    input [7:0] In;
+    output [7:0] Out;
+    
+    wire [7:0] MulBy8Out;
+
+    GFMulBy8 tu0(Clk, In, MulBy8Out);
+
+    assign Out = MulBy8Out ^ In; 
+endmodule 
+
+// Takes 2 Cycles
+module GFMulBy0xB(Clk, In, Out);
+    input Clk;
+    input [7:0] In;
+    output [7:0] Out;
+
+    wire [7:0] MulBy8Out, MulBy2Out;
+    GFMulBy8 tu0(Clk, In, MulBy8Out);
+    GFMulBy2 tu1(Clk, In, MulBy2Out);
+
+    assign Out = MulBy8Out ^ MulBy2Out ^ In;
+endmodule
+
+// Takes 2 Cycles
+module GFMulBy0xD(Clk, In, Out);
+    input Clk;
+    input [7:0] In;
+    output [7:0] Out;
+
+    wire [7:0] MulBy8Out, MulBy4Out;
+
+    GFMulBy8 tu0(Clk, In, MulBy8Out);
+    GFMulBy4 tu1(Clk, In, MulBy4Out);
+
+    assign Out = MulBy8Out ^ MulBy4Out ^ In;
+endmodule
+
+// Takes 2 Cycles
+module GFMulBy0xE(Clk, In, Out);
+    input Clk;
+    input [7:0] In;
+    output [7:0] Out;
+
+    wire [7:0] MulBy8Out, MulBy4Out, MulBy2Out;
+
+    GFMulBy2 tu0(Clk, In, MulBy2Out);
+    GFMulBy4 tu1(Clk, In, MulBy4Out);
+    GFMulBy8 tu2(Clk, In, MulBy8Out);
+
+    assign Out = MulBy2Out ^ MulBy4Out ^ MulBy8Out;
+
+endmodule
 
